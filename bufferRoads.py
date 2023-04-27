dccf560b3143cb14bed152a7d6eb0af918ce2925
Jbello <Jbello@uoguelph.ca>
2023-04-27 08:08:24 -0400

    Module1 done

--- bufferRoads.py
+++ bufferRoads.py
@@ -1,8 +1,28 @@
 """
 Set interpreter to: C:\Python27\ArcGIS10.7
 """
+from inspect import Attribute
+from os import access
 import arcpy
 # Set geoprocessing environments
-arcpy.env
-# arcpy.env.overwriteOutput = True
+arcpy.env.workspace = r"PythonGP\PythonGP\Data\SanJuan.gdb"
+arcpy.env.overwriteOutput = True
+
+# Set parameter used to join the BufferDistance table to the Road feature class
+# print(arcpy.Usage("JoinField_management"))
+inFeatures = "Roads"
+inField = "ROUTE_TYPE"
+joinTable = "BufferDistance"
+joinField = "ROUTE_TYPE"
+ 
+# Join table to feature class
+arcpy.JoinField_management(inFeatures, inField, joinTable, joinField)
+
+# Set parameter for the buffer Road feature class
+# print(arcpy.Usage("Buffer_analysis"))
+outBuffers = "RoadBuffers"
+buffField = "DISTANCE"
+
+# Buffer the roads based on DISTANCE Attribute
+arcpy.Buffer_analysis(inFeatures, outBuffers, buffField)
 print("Hello, world")
